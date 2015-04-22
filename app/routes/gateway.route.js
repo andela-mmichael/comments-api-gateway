dedfb894e5a72b6ed695f05811ba0988d01f28f8
mel <mel.michael@andela.co>
2015-04-22 12:42:26 +0100

    Implement api routes

--- app/routes/gateway.route.js
+++ app/routes/gateway.route.js
@@ -1,1 +1,18 @@
-gateway.route.js
+var Controller = require('../controllers/gateway.controller');
+var express = require('express');
+var Router = express.router();
+
+Router
+  .post('/signup', Controller.signUp)
+
+  .post('/login', Controller.login)
+
+  .get('/:users/comments')
+
+  .post('/:users/comments')
+
+  .get('/:users/comments/:name')
+
+  .put('/:users/comments/:name')
+
+  .delete('/:users/comments/:name');
